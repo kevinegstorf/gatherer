0756fd385f9613ab1209ed16163d1f2ca6dc8d0f
kevinegstorf <kevin.egstorf@gmail.com>
2016-09-30 16:48:14 +0200

    added more tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,35 @@ class Project
     @tasks = []
   end
 
+  def incomplete_tasks
+    tasks.reject(&:complete?)
+  end
+
   def done?
-    tasks.reject(&:complete?).empty?
+  incomplete_tasks.empty?
+  end
+
+  def total_size
+    tasks.sum(&:size)
+  end
+
+  def remaining_size
+    incomplete_tasks.sum(&:size)
+  end
+
+  def completed_velocity
+    task.sum(&:points_toward_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0 / 21
+  end
+
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
+
+  def on_schedule?
+    (Date.today + projected_days_remaining) <= due_date
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,14 +1,25 @@
-# Task Class
 class Task
-  def initialize
-    @completed = false
+  attr_accessor :size, :completed_at
+
+  def initialize(options = {})
+    mark_completed(options[:completed_at]) if options[:completed_at]
+    @size = options[:size]
   end
 
-  def mark_completed
-    @completed = true
+  def mark_completed(date = nil)
+    @completed_at = (date || Time.current)
   end
 
   def complete?
-    @completed
+    completed_at.present?
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    completed_at > 3.weeks.ago
+  end
+
+  def points_toward_velocity
+    if part_of_velocity? then size else 0 end
   end
 end
