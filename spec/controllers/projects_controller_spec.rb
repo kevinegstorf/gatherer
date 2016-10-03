5a72c069b3c371b79b6cde0ef4eb9546b484f7a5
kevinegstorf <kevin.egstorf@gmail.com>
2016-10-03 20:57:57 +0200

    added projects controller spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe ProjectsController, type: :controller do
+describe ProjectsController, type: :controller do
+  describe 'POST create' do
+    it 'creates a project' do
+      post :create, project: { name: 'Runway', tasks: 'Startsomething:2' }
 
+      expect(response).to redirect_to(projects_path)
+      expect(assigns(:action).project.name).to eq('Runway')
+    end
+  end
 end
